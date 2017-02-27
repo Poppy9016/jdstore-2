141d26dcb0f528939730615e257258763c61484c
huckssr <huckssr@gmail.com>
2017-02-27 23:11:59 +0800

    新增搜索功能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,12 +5,16 @@ class ProductsController < ApplicationController
 
     def index
         @products = Product.all
+        if params[:search]
+            @products = Product.search(params[:search]).order('created_at DESC')
+        else
+            @products = Product.all.order('created_at DESC')
+          end
    end
 
     def show
         @product = Product.find(params[:id])
-  end
-
+      end
 
     def add_to_cart
         @product = Product.find(params[:id])
