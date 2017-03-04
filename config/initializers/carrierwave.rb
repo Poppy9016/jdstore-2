961b98414d29fb942b7a06995d7ccd18e3a29ce6
huckssr <huckssr@gmail.com>
2017-03-04 16:04:30 +0800

    完成稿9

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,4 +8,7 @@ CarrierWave.configure do |config|
     config.qiniu_block_size    = 4 * 1024 * 1024
     config.qiniu_protocol      = 'http'
     config.qiniu_up_host       = 'http://up.qiniug.com' # 选择不同的区域时，"up.qiniug.com" 不同
+  else
+config.storage :file
+end
 end
